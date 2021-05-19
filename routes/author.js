b12591ef38b720ce85a8b6ed351550036d7fc150
arnzero <afh146@humboldt.edu>
2021-05-19 12:15:58 -0700

    fixed few errors

--- routes/author.js
+++ routes/author.js
@@ -6,11 +6,11 @@ const Author = require('../models/author')
 // All Authors Route
 router.get('/', async (req, res) => {
     let searchOptions = {}
-    if (req.query.name != null && req.query.name != '') {
+    if (req.query.name != null && req.query.name !== '') {
         searchOptions.name = new RegExp(req.query.name, 'i')
     }
     try {
-        const authors = await Author.find({searchOptions})
+        const authors = await Author.find(searchOptions)
         res.render('authors/index', {
             authors: authors,
             searchOptions: req.query })// res.send('Hello World')
